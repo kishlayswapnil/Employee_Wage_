18b40120d72e49cc55f0e4f660db5fa96c88662b
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-14 13:03:01 +0530

    Added: Employee is present or absent is being checked.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,15 @@
 #Welcome To Problem Statements Of Employee Wage.
+
+#!/bin/bash -x
+#usecase being solved showing employee is present or not.
+
+#Declaring Varriables.
+IsAbsent=0
+AttendanceCheck=$((RANDOM%2))
+#Logic for checking employee is present or not.
+if (( AttendanceCheck == 1 ))
+then
+	echo Employee is Present
+else
+	echo Employee is Absent
+fi
