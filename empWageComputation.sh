80c93f09b0948f826921ed3e8ab8e8be9ee3b434
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-15 16:13:43 +0530

    Added: Stored daily wage here.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,25 +1,19 @@
-##!/bin/bash -x
+#!/bin/bash -x
 echo Welcome To Employee Wage problem.
 
 #usecase to show the wage if day or working hour which ever comes first with the help of function.
-#Declaring constants.
-WAGEHOUR=20
-CONDITIONHOUR=100
-#Logic for checking the condition under the function.
-function work() {
-	#Declaring Constants.
-	MONTH=20
 
-	#Declaring Varriables.
-	wagePerHour=$1
-   HourCondition=$2
-	day=0
-	hour=0
-	while(( day<=MONTH && hour<=HourCondition))
-	do
-		randomValue=$((RANDOM%3))
-		((day++))
-		case $randomValue in
+#Declaring constants.
+WAGEPERHOUR=20
+HOURCONDITION=100
+MONTH=20
+#Declaring Varriables.
+day=0
+hour=0
+
+#Logic for checking the working hour under the function.
+function workHour() {
+	case $1 in
 			1)
 				workingHour=8
 			;;
@@ -30,13 +24,23 @@ function work() {
 				workingHour=0
 			;;
 		esac
-		hour=$((hour+workingHour))
-	done
-	#Printing the total salary.
-	salary=$(($wagePerHour*$hour))
-	echo "Total Salary will be: $salary"
+		echo $workingHour
 }
+function dailyWage() {
+	local timeHour=$1
+	dailyIncome=$(($timeHour*$WAGEPERHOUR))
+	echo $dailyIncome
+}
+while(( day<MONTH && hour<HOURCONDITION ))
+do
+	randomValue=$((RANDOM%3))
+	emphour=$(workHour $randomValue)
+	hour=$(($hour+$emphour))
+	wageArray[day]=$(dailyWage $emphour)
+	((day++))
+done
 
-#Displaying the function.
-result=$(work $WAGEHOUR $CONDITIONHOUR)
-	echo $result
+#Printing the total salary.
+salary=$(($WAGEPERHOUR*$hour))
+	echo "Total Salary will be: $salary"
+	echo "Daily income: ${wageArray[@]}"
