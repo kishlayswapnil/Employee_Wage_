a0a57bad0708b11e308ed1cb08e8898b39dcbb98
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-15 13:14:05 +0530

    Refactor: Added a function that takes input as wage per hour and hour condition as inputs.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,41 +2,38 @@
 echo Welcome To Employee Wage problem.
 
 #usecase to show the wage if day or working hour which ever comes first with the help of function.
+
 #Declaring constants.
-WAGEHOUR=20
-CONDITIONHOUR=100
-#Logic for checking the condition under the function.
-function work() {
-	#Declaring Constants.
-	MONTH=20
+WAGEPERHOUR=20
+HOURCONDITION=100
+MONTH=20
 
-	#Declaring Varriables.
-	wagePerHour=$1
-	HourCondition=$2
-	day=0
-	hour=0
-	while(( day<=MONTH && hour<=HourCondition))
-	do
-		randomValue=$((RANDOM%3))
-		((day++))
-		case $randomValue in
-			1)
-				workingHour=8
-			;;
-			2)
-				workingHour=4
-			;;
-			*)
-				workingHour=0
-			;;
-		esac
-		hour=$((hour+workingHour))
-	done
-	#Printing the total salary.
-	salary=$(($wagePerHour*$hour))
-	echo "Total Salary will be: $salary"
+#Declaring Varriables.
+day=0
+hour=0
+#Logic for checking the working hour under the function.
+function workHour() {
+	case $1 in
+         1)
+            workingHour=8
+         ;;
+         2)
+            workingHour=4
+         ;;
+         *)
+            workingHour=0
+         ;;
+      esac
+		echo $workingHour
 }
+while(( day<MONTH && hour<HOURCONDITION ))
+do
+	randomValue=$((RANDOM%3))
+	((day++))
+	emphour=$(workHour $randomValue)
+	hour=$(($hour+$emphour))
+done
 
-#Displaying the function.
-result=$(work $WAGEHOUR $CONDITIONHOUR)
-	echo $result
+#Printing the total salary.
+salary=$(($WAGEPERHOUR*$hour))
+	echo "Total Salary will be: $salary"
