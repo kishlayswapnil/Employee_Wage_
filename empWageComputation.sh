d28f377114d893372bef089fa05fe7aa85d62cf3
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-15 09:48:28 +0530

    Added: Calculation for monthly wage has been added.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -x
 echo Welcome To Employee Wage problem.
 
-#usecase to show the daily wage of part time enployee and fulltime employee using case statement.
+#usecase to show the wage for month+ of part time enployee and fulltime employee using case statement.
 #Declaring Varriables.
 randomValue=$((1+RANDOM%2))
-fullTime=1
 wagePerHour=20
+workingDay=20
 #Logic for checking the respective wages of fulltime and parttime employee.
 case $randomValue in
 	1)
-		totalWage=$((wagePerHour*8))
-		echo "Employee is present and his daily wage is $totalWage"
+		workingHour=8
+		echo "Employee works fulltime and his daily wage is: "
 	;;
 	2)
-		partimeWage=$((wagePerHour*4))
-		echo "Employee is present and he works part time and his daily wage is $partimeWage"
+		workingHour=4
+		echo "Employee works part time and his daily wage is: "
 	;;
 esac
+salary=$((workingDay*wagePerHour*$workingHour))
+echo $salary
