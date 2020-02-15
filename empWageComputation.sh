e284750f81f1ebf024b6fda4f2e63119e2c60eda
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-15 12:00:25 +0530

    Added: The check of condition is being performed between working hour and days.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,35 @@
-#!/bin/bash -x
+##!/bin/bash -x
 echo Welcome To Employee Wage problem.
 
-#usecase to show the wage for month+ of part time enployee and fulltime employee using case statement.
+#usecase to show the wage if day or working hour which ever comes first.
+#Declaring Constants.
+WAGEPERHOUR=20
+HOURCONDITION=100
+MONTH=20
+
 #Declaring Varriables.
-randomValue=$((1+RANDOM%2))
-wagePerHour=20
-workingDay=20
+day=0
+hour=0
+
 #Logic for checking the respective wages of fulltime and parttime employee.
-case $randomValue in
-	1)
-		workingHour=8
-		echo "Employee works fulltime and his daily wage is: "
-	;;
-	2)
-		workingHour=4
-		echo "Employee works part time and his daily wage is: "
-	;;
-esac
-salary=$((workingDay*wagePerHour*$workingHour))
-echo $salary
+while(( day<=MONTH && hour<=HOURCONDITION))
+do
+	randomValue=$((RANDOM%3))
+	((day++))
+	case $randomValue in
+		1)
+			workingHour=8
+		;;
+		2)
+			workingHour=4
+		;;
+		*)
+			workingHour=0
+		;;
+	esac
+	hour=$((hour+workingHour))
+done
+
+#Printing the total salary.
+salary=$(($WAGEPERHOUR*$hour))
+echo "Total Salary will be: $salary"
